36b241e4b2eb1326ad8698acdcc7c499644843df
Sergey-15-r <sergey.alikov.98@mail.ru>
2020-04-25 15:46:26 +0300

    проба2))

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 "use strict";
-const userRank = () => {
+const createStatisticrRank = () => {
   return (
     `<section class="statistic">
     <p class="statistic__rank">
@@ -49,3 +49,37 @@ const userRank = () => {
   </section> `
   );
 };
+
+const createProfileWindow = () => {
+  return (
+    `<section class="header__profile profile">
+  <p class="profile__rating">Movie Buff</p>
+  <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+ </section>`
+  );
+};
+
+const createMainNavigation = () => {
+  return (
+    `<nav class="main-navigation">
+    <div class="main-navigation__items">
+      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+    </div>
+    <a href="#stats" class="main-navigation__additional">Stats</a>
+  </nav>`
+  );
+};
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteHeaderElement = document.querySelector(`.header`);
+const siteMainElement = document.querySelector(`.main`);
+
+render(siteMainElement, createStatisticRank(), `beforeend`);
+render(siteMainElement, createProfileWindow(), `beforeend`);
+render(siteMainElement, createMainNavigation(), `beforeend`);
